f4e87326e16e126607aa270fc0dcf5b30fd82aa3
Valerie <valerie.tram.anh.n@gmail.com>
2021-11-13 15:23:24 -0500

    add CRUD routes

---
+++ server.js
@@ -0,0 +1,81 @@
+const express = require('express')
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const mysql = require('mysql2');
+const inputCheck = require('./utils/inputCheck');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '',
+      database: 'tracker'
+    },
+    console.log('Connected to the tracker database.')
+);
+
+app.get('/api/employees', (req, res) => {
+    const sql = `SELECT * FROM employees`;
+  
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+});
+
+// Get a single employee
+app.get('/api/employees/:id', (req, res) => {
+    const sql = `SELECT * FROM employees WHERE id = ?`;
+    const params = [req.params.id];
+  
+    db.query(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+});
+
+// Create an employee
+app.post('/api/employees', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'role_id', 'manager_id');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+    const sql = `INSERT INTO candidates (first_name, last_name, role_id, manager_id) VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.role_id, body.manager_id]  ;
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
